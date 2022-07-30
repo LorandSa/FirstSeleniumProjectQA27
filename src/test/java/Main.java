01d6326de5dfb977e8749f7a0b4b5e08c205cda4
LorandSa <lorand.sabau@yahoo.com>
2022-07-30 17:22:51 +0300

    LAB 16 Tema Commit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,12 +2,13 @@
 public class Main {
 
     public static void main(String[] args) {
-      LoginTest loginTest = new LoginTest();
-      loginTest.validLoginTest();
-
-
+//      LoginTest loginTest = new LoginTest();
+//      loginTest.validLoginTest(); //9. Login
 //      RegisterTest registerTest = new RegisterTest();
-//      registerTest.validRegisterTest();
+//      registerTest.validRegisterTest(); //8. Register
+        WishListTest wishListTest = new WishListTest();
+        //wishListTest.accessWishListTest(); //3. Wishlist
+        wishListTest.addToWishListValidTest(); //5. WishlistAddProduct
 
     }
 
---
+++ src/test/java/WishListTest.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WishListTest {
+    public void accessWishListTest(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(2) > a")).click();
+        driver.findElement(By.id("email")).sendKeys("test@testQA27.ro");
+        driver.findElement(By.id("pass")).sendKeys("123456");
+        driver.findElement(By.id("send2")).click();
+
+        wait(5);
+        driver.quit();
+    }
+    public void addToWishListValidTest(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.id("email")).sendKeys("test@testQA27.ro");
+        driver.findElement(By.id("pass")).sendKeys("123456");
+        driver.findElement(By.id("send2")).click();
+        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a")).click();
+        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li > div > div.actions > ul > li:nth-child(1) > a")).click();
+
+        wait(5);
+        driver.quit();
+    }
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
